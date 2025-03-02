308fc0442581bce4f620b3e0fa0fddb3ffc15ecc
Rajveer Dangi <dangirajveer33@gmail.com>
2025-03-02 18:49:58 +0530

    Updated files

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/matchable/", // Set for root deployment
+  base: "/matchable/",
 }));
